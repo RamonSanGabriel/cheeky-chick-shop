dcc79a1cf7db4ffee99f73088779480a2a9dc202
RamonSanGabriel <jeshero11@gmail.com>
2025-01-11 01:03:16 +0800

    information.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -9,61 +9,63 @@ const NavBar = () => {
   const { isOpen: isSidebarOpen, toggle: toggleSidebar } = useToggle(false);
 
   return (
-    <nav className={css.navBar}>
-      <p className={css.navHome}>
-        <a className={css.link} href="#">
-          Home
-        </a>
-      </p>
-
-      <div>
-        <form role="search">
-          <input
-            className={css.navSearch}
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          {/*     <button className={css.link} type="submit">
-            Search
-          </button> */}
-        </form>
-      </div>
-
-      <div className={css.dropDown}>
-        <p>
+    <div className={css.navBarContainer}>
+      <nav className={css.navBar}>
+        <p className={css.navHome}>
           <a className={css.link} href="#">
-            User
-            {/* <i className={css.faCaretDown}></i> */}
+            Home
           </a>
         </p>
-        <div className={css.dropdownContent}>
-          <ul className={css.linkList}>
-            {navLinks.map(({ id, link, path }) => (
-              <li key={id}>
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? css.linkActive : css.link
-                  }
-                  to={path}
-                >
-                  {link}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
+
+        <div>
+          <form role="search">
+            <input
+              className={css.navSearch}
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            {/*     <button className={css.link} type="submit">
+            Search
+          </button> */}
+          </form>
         </div>
-      </div>
 
-      <MdOutlineMenu
-        className={`${css.hamburgerIcon} ${
-          isSidebarOpen && css.visuallyHidden
-        }`}
-        onClick={toggleSidebar}
-      />
+        <div className={css.dropDown}>
+          <p>
+            <a className={css.link} href="#">
+              User
+              {/* <i className={css.faCaretDown}></i> */}
+            </a>
+          </p>
+          <div className={css.dropdownContent}>
+            <ul className={css.linkList}>
+              {navLinks.map(({ id, link, path }) => (
+                <li key={id}>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? css.linkActive : css.link
+                    }
+                    to={path}
+                  >
+                    {link}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
 
-      <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
-    </nav>
+        <MdOutlineMenu
+          className={`${css.hamburgerIcon} ${
+            isSidebarOpen && css.visuallyHidden
+          }`}
+          onClick={toggleSidebar}
+        />
+
+        <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
+      </nav>
+    </div>
   );
 };
 
